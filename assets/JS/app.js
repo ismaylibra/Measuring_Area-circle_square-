250aab61fe09a7de44cfe5cdd705cb74e93c36b6
abbasoov <ismayiliib@code.edu.az>
2022-07-01 14:09:33 +0400

    added homework

---
+++ assets/JS/app.js
@@ -0,0 +1,7 @@
+//Area of the Circle
+var radius = 15
+console.log('Area of Circle: '+ (Math.floor(Math.PI)*Math.pow(radius,2)));
+
+// Area of the Square
+var length = 12
+console.log('Area of the Square: ' + Math.pow(length,2));
